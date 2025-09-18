e7cd5b51e895068817bbfb5d5b92f62ef538f797
Balaraj R <balarajr483@gmail.com>
2025-09-18 12:38:37 +0000

    i am getting this error for many times and the profile is not saving Err

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyAZRQLIieXFytt1ztD8uE6TeaqeT4ggBAs",
   authDomain: "careerlens-1.firebaseapp.com",
   projectId: "careerlens-1",
-  storageBucket: "careerlens-1.firebasestorage.app",
+  storageBucket: "careerlens-1.appspot.com",
   messagingSenderId: "202306950137",
   appId: "1:202306950137:web:ed4e91e619dd4cc7dde328",
   measurementId: "G-WEF48JHJF9"
